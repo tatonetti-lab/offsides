46ac9b8af515c5be97119fb55ff82bac1cf4da6b
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2022-11-16 18:59:14 -0500

    Added tempalte for faers processor Added data directory to gitignore

---
+++ src/faers_processor.py
@@ -0,0 +1,17 @@
+"""
+Manage the latest available FDA Adverse Event Reporting System downloads.
+
+"""
+
+import os
+import sys
+import csv
+import requests
+import argparse
+
+def main():
+    
+    pass
+
+if __name__ == '__main__':
+    main()
